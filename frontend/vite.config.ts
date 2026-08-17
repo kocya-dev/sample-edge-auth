66761c3787e0b2b4495bcdea5476917abd503114
kocya <sample@test.co.jp>
2026-08-17 23:12:44 +0900

    update packages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), basicSsl()],
+  plugins: [vue(), vueJsx(), basicSsl()],
   server: {
     https: true,
   },
